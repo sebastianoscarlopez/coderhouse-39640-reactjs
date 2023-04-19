378c0e9e0d104f0f67238626188794fec7d2e4c1
Sebastian Oscar Lopez <sebastianoscarlopez@gmail.com>
2023-04-18 22:00:47 -0300

    clase-11 context Jar money

--- src/components/ContextPoc/GrandSonOfApp.js
+++ src/components/ContextPoc/GrandSonOfApp.js
@@ -1,9 +1,14 @@
+import { useContext } from "react";
 import GreatGrandSonOfApp from "./GreatGrandSonOfApp";
+import { MoneyContext } from "./ContextPoc";
 
 const GrandSonOfApp = () => {
+  const { addMoney }  = useContext(MoneyContext);
+
   return (
     <div>
       <h1>Grand Son of App</h1>
+      <button onClick={addMoney}>Save Money $ 1</button>
       <GreatGrandSonOfApp />
     </div>
   );
--- src/components/ContextPoc/GreatGrandSonOfApp.js
+++ src/components/ContextPoc/GreatGrandSonOfApp.js
@@ -3,12 +3,11 @@ import { JarMoney } from "./JarMoney";
 import { MoneyContext } from "./ContextPoc";
 
 const GreatGrandSonOfApp = () => {
-  const { money, addMoney }  = useContext(MoneyContext);
+  const { money } = useContext(MoneyContext);
 
   return (
     <div>
       <h1>Great Grand Son of App</h1>
-      <button onClick={addMoney}>Save Money $ 1</button>
       <JarMoney>{money}</JarMoney>
     </div>
   );
